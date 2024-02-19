d4e6008270bfd0f6ef5ca0ae58d762b8dc0aac00
jlmiller12s <jlmiller12s@gmail.com>
2024-02-19 14:31:07 -0600

    styles fix

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -16,6 +16,19 @@ export default async function decorate(block) {
   // decorate footer DOM
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+   // setup image columns
+   [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add('columns-img-col');
+        }
+      }
+    });
+  });
 
   block.append(footer);
 }
